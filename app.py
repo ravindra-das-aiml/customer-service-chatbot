4db1f3546b460a26817c44a4ecc3e319a91b176e
Ravindra Das <ravindra21075@gmail.com>
2026-06-15 14:34:51 +0530

    fix streamlit cloud path issue

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-import streamlit as st
-import json
+import os
 import pickle
-import random
+import streamlit as st
 
-model = pickle.load(open("chatbot_model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+BASE_DIR = os.path.dirname(__file__)
 
-with open("intents.json") as file:
-    intents = json.load(file)
+model_path = os.path.join(BASE_DIR, "chatbot_model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "vectorizer.pkl")
 
-st.title("Customer Service Chatbot 🤖")
-
-user_input = st.text_input("Ask something")
-
-if user_input:
-    X = vectorizer.transform([user_input])
-    prediction = model.predict(X)[0]
-
-    for intent in intents["intents"]:
-        if intent["tag"] == prediction:
-            st.success(random.choice(intent["responses"]))
+model = pickle.load(open(model_path, "rb"))
+vectorizer = pickle.load(open(vectorizer_path, "rb"))
